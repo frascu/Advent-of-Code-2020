fa72d922936bb5cd7ddd29cdf71220f3a5d6cbd4
frascu <francescoscuccimarri@gmail.com>
2020-12-02 00:56:16 +0100

    remove html tags

--- src/it/frascu/adaventcode/Day01.java
+++ src/it/frascu/adaventcode/Day01.java
@@ -4,6 +4,23 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
+/**
+ * https://adventofcode.com/2020/day/1
+ *
+ * --- Day 1: Report Repair ---
+ * After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island.
+ * Surely, Christmas will go on without you.
+ *
+ * The tropical island has its own currency and is entirely cash-only.
+ * The gold coins used there have a little picture of a starfish; the locals just call them stars.
+ * one of the currency exchanges seem to have heard of them,
+ * but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
+ *
+ * To save your vacation, you need to get all fifty stars by December 25th.
+ *
+ * Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar;
+ * the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+ */
 public class Day01 {
 
     private static final String INPUT_FILE_PATH = "input/day01-input.txt";
@@ -15,39 +32,25 @@ public class Day01 {
     }
 
     /**
-     * @see <a href="https://adventofcode.com/2020/day/1">Day 1</a>
-     * <p>
-     * --- Day 1: Report Repair ---
-     * After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island.
-     * Surely, Christmas will go on without you.
-     * <p>
-     * The tropical island has its own currency and is entirely cash-only.
-     * The gold coins used there have a little picture of a starfish; the locals just call them stars.
-     * one of the currency exchanges seem to have heard of them,
-     * but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
-     * <p>
-     * To save your vacation, you need to get all fifty stars by December 25th.
-     * <p>
-     * Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar;
-     * the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
-     * <p>
+     * --- Part One ---
+     *
      * Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input);
      * apparently, something isn't quite adding up.
-     * <p>
+     *
      * Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
-     * <p>
+     *
      * For example, suppose your expense report contained the following:
-     * <p>
+     *
      * 1721
      * 979
      * 366
      * 299
      * 675
      * 1456
-     * <p>
+     *
      * In this list, the two entries that sum to 2020 are 1721 and 299. M
-     * ultiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
-     * <p>
+     * multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+     *
      * Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
      */
     private static void partOne() {
@@ -73,10 +76,10 @@ public class Day01 {
      * The Elves in accounting are thankful for your help;
      * one of them even offers you a starfish coin they had left over from a past vacation.
      * They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
-     * <p>
+     *
      * Using the above example again, the three entries that sum to 2020 are 979, 366, and 675.
      * Multiplying them together produces the answer, 241861950.
-     * <p>
+     *
      * In your expense report, what is the product of the three entries that sum to 2020?
      */
     private static void partTwo() {
